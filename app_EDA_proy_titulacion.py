d4bb2e8e6c2c1c12c9f230ff9d38690809305c3c
Rodrigo Guarneros <rodrigoguarneros330@hotmail.com>
2025-05-03 20:29:36 -0600

    Final14

--- app_EDA_proy_titulacion.py
+++ app_EDA_proy_titulacion.py
@@ -159,9 +159,7 @@ def bajando_procesando_datos():
     # Convertir a categoría con orden específico
     datos['Etapa_Madurez'] = pd.Categorical(
         datos['Etapa_Madurez'],
-        categories=categorias_orden,
-        ordered=True
-    )
+        categories=categorias_orden)
     
     return datos
 
@@ -1042,19 +1040,19 @@ def generar_grafico_3d_con_lugar(df, df_normalizado, dataset_complete, lugar_sel
 
     # Crear DataFrame para Plotly
     pca_df = pd.DataFrame(df_pca2, columns=['PCA1', 'PCA2', 'PCA3'])
-    pca_df['Madurez'] = df['Etapa_Madurez']  # Usar la versión categorizada
+    pca_df['Etapa_Madurez'] = df['Etapa_Madurez']  # Usar la versión categorizada
     pca_df['Lugar'] = dataset_complete['Lugar']
 
     # Crear el gráfico asegurando el orden y los colores
     fig = px.scatter_3d(
         pca_df, 
         x='PCA1', y='PCA2', z='PCA3',
-        color='Madurez',
+        color='Etapa_Madurez',
         labels={'PCA1': 'Componente PC1', 
                 'PCA2': 'Componente PC2', 
                 'PCA3': 'Componente PC3'},
         hover_data=['Lugar'],
-        category_orders={'Madurez': ['Optimización', 'Definición', 'En desarrollo', 'Inicial']},
+        category_orders={'Etapa_Madurez': ['Optimización', 'Definición', 'En desarrollo', 'Inicial']},
         color_discrete_map=color_map
     )
     # Manejar lugar seleccionado
@@ -1132,8 +1130,6 @@ grafico3d = generar_grafico_3d_con_lugar(datos, df_normalizado, dataset_complete
 
 
 def generar_grafico_2d(df, df_normalizado, dataset_complete, lugar_seleccionado=None):
-    # Asegurarse de que no haya espacios extras o diferencias de capitalización
-    df['Madurez'] = df['Madurez'].str.strip()  # Eliminar espacios
     
     # Normalización de PCA
     df_pca2 = df_normalizado.to_numpy()
@@ -1141,7 +1137,7 @@ def generar_grafico_2d(df, df_normalizado, dataset_complete, lugar_seleccionado=
 
     # Crear DataFrame para Plotly
     pca_df = pd.DataFrame(df_pca2, columns=['PCA1', 'PCA2', 'PCA3'])
-    pca_df['Madurez'] = df['Madurez'].astype('category')
+    pca_df['Etapa_Madurez'] = df['Etapa_Madurez'].astype('category')
     pca_df['Lugar'] = dataset_complete['Lugar']
 
     # Definir un mapa de colores estricto
@@ -1155,11 +1151,11 @@ def generar_grafico_2d(df, df_normalizado, dataset_complete, lugar_seleccionado=
     # Crear el gráfico de dispersión 2D
     fig = px.scatter(pca_df, 
                      x='PCA1', y='PCA2',
-                     color='Madurez',
+                     color='Etapa_Madurez',
                      labels={'PCA1': 'Componente PC1', 
                             'PCA2': 'Componente PC2'},
                      hover_data=['Lugar'],
-                     category_orders={'Madurez': ['Optimización', 'Definición', 'En desarrollo', 'Inicial']},  # Orden explícito
+                     category_orders={'Etapa_Madurez': ['Optimización', 'Definición', 'En desarrollo', 'Inicial']},  # Orden explícito
                      color_discrete_map=color_map)
 
     # Manejar lugar seleccionado
@@ -1214,7 +1210,7 @@ grafico2d1 = generar_grafico_2d(df, df_normalizado, dataset_complete, lugar_sele
 
 def generar_grafico_2d2(df, df_normalizado, dataset_complete, lugar_seleccionado=None):
     # Limpiar posibles espacios o caracteres invisibles en 'Madurez'
-    df['Madurez'] = df['Madurez'].astype('category')
+    df['Etapa_Madurez'] = df['Etapa_Madurez'].astype('category')
     
     # Normalización de PCA
     df_pca2 = df_normalizado.to_numpy()
@@ -1222,7 +1218,7 @@ def generar_grafico_2d2(df, df_normalizado, dataset_complete, lugar_seleccionado
 
     # Crear DataFrame para Plotly
     pca_df = pd.DataFrame(df_pca2, columns=['PCA1', 'PCA2', 'PCA3'])
-    pca_df['Etapa_Madurez'] = df['Madurez']
+    pca_df['Etapa_Madurez'] = df['Etapa_Madurez']
     pca_df['Lugar'] = dataset_complete['Lugar']
 
     # Definir un mapa de colores más contrastante
@@ -1298,8 +1294,6 @@ grafico2d2 = generar_grafico_2d2(datos, df_normalizado, dataset_complete, lugar_
 
 
 def generar_grafico_2d3(df, df_normalizado, dataset_complete, lugar_seleccionado=None):
-    # Asegurarse de que no haya espacios extras o diferencias de capitalización
-    df['Madurez'] = df['Madurez'].str.strip()  # Eliminar espacios
     
     # Normalización de PCA
     df_pca2 = df_normalizado.to_numpy()
@@ -1307,7 +1301,7 @@ def generar_grafico_2d3(df, df_normalizado, dataset_complete, lugar_seleccionado
 
     # Crear DataFrame para Plotly
     pca_df = pd.DataFrame(df_pca2, columns=['PCA1', 'PCA2', 'PCA3'])
-    pca_df['Madurez'] = df['Madurez'].astype('category')
+    pca_df['Etapa_Madurez'] = df['Etapa_Madurez'].astype('category')
     pca_df['Lugar'] = dataset_complete['Lugar']
 
     # Definir un mapa de colores estricto
@@ -1321,11 +1315,11 @@ def generar_grafico_2d3(df, df_normalizado, dataset_complete, lugar_seleccionado
     # Crear el gráfico de dispersión 2D
     fig = px.scatter(pca_df, 
                      x='PCA2', y='PCA3',
-                     color='Madurez',
+                     color='Etapa_Madurez',
                      labels={'PCA2': 'Componente PC2', 
                             'PCA3': 'Componente PC3'},
                      hover_data=['Lugar'],
-                     category_orders={'Madurez': ['Optimización', 'Definición', 'En desarrollo', 'Inicial']},  # Orden explícito
+                     category_orders={'Etapa_Madurez': ['Optimización', 'Definición', 'En desarrollo', 'Inicial']},  # Orden explícito
                      color_discrete_map=color_map)
 
     # Manejar lugar seleccionado
